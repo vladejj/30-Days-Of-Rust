e70b3d1a81ea8e4a05523ca431846ba14238a47b
vladejj <98359261+vladejj@users.noreply.github.com>
2025-08-12 23:39:54 +0200

    Some prints

--- 01_Intro_to_rust/hello-test/src/main.rs
+++ 01_Intro_to_rust/hello-test/src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    let name = "Vladejj";
+    let reason = "I'm excited to learn Rust because I heard it's good and I want to learn new things!";
+    println!("Hello, my name is {} and {}.", name, reason);
 }
